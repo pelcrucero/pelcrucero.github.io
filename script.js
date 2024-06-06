540d02397b2fbb241483dbd3898a30165886c536
pelcrucero <isracrucero@hotmail.com>
2024-06-06 15:31:16 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -9,8 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
             { name: 'Patatas / Chaskis', price: 1, quantity: 0 }
         ],
         bebida: [
-            { name: 'Caña / Caña con Limón', price: 1.3, quantity: 0 },
-            { name: 'Calimocho', price: 1.3, quantity: 0 },
+            { name: 'Caña / Cali', price: 1.3, quantity: 0 },
             { name: 'Vino', price: 1.3, quantity: 0 },
             { name: 'Cachi', price: 4.5, quantity: 0 },
             { name: 'Refresco', price: 1.7, quantity: 0 },
